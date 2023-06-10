7b1f2bdea55475ad50adc954a9093b530fb271f6
gentian-behrami <gb41535@ubt-uni.net>
2023-06-10 22:04:26 +0200

    calculator slimlit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input('Enter number: '))
+num2 = int(input('Enter number: '))
+
+def add_two(num_1,num_2):
+    result = num_1 + num_2
+    return result
+
+total = add_two(num1,num2)
+
+print('The result is: ',total)  
