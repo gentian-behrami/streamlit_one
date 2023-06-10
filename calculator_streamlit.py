39ef84f120afe06f73ee023c6be8266170d7f215
gentian-behrami <gb41535@ubt-uni.net>
2023-06-10 23:18:45 +0200

    Add more features fore calculator

--- calculator_streamlit.py
+++ calculator_streamlit.py
@@ -9,9 +9,33 @@ def add_two(num_1,num_2):
     result = num_1 + num_2
     return result
 
+def min_two(num_1,num_2):
+    result = num_1 - num_2
+    return result
+
+def mul_two(num_1,num_2):
+    result = num_1 * num_2
+    return result
+
+def div_two(num_1,num_2):
+    result = num_1 / num_2
+    return result
 # total = add_two(num1,num2)
 
-if st.button("Calculate the result"):
+if st.button("Calculate sum of numbers"):
     total = add_two(num1,num2)
     st.write(f'The result is {total} ')
+elif st.button("Calculate substraction of numbers"):
+    total = min_two(num1,num2)
+    st.write(f'The result is {total}')
+elif st.button("Calculate multiply of numbers"):
+    total = mul_two(num1,num2)
+    st.write(f'The result is {total}')
+elif st.button("Calculate division of numbers"):
+    if num2 == 0:
+        st.write('Cannot divide by zero')
+    else:
+        total = div_two(num1,num2)
+        st.write(f'The result is {total}')
+
 
