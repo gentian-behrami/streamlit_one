9ce0888d74b0a6c69cb9b8f7b38e3edab0aff628
gentian-behrami <gb41535@ubt-uni.net>
2023-06-10 22:47:46 +0200

    streamlit calculator

--- calculator_streamlit.py
+++ calculator_streamlit.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
-number = st.number_input('Enter a number')
-st.write('The current number is: ',number)  
+st.title('Our streamlite calculator app ')
+
+num1 = st.number_input('Please enter a number',key=1)
+num2 = st.number_input('Please enter a number',key=2)
+
+def add_two(num_1,num_2):
+    result = num_1 + num_2
+    return result
+
+# total = add_two(num1,num2)
+
+if st.button("Calculate the result"):
+    total = add_two(num1,num2)
+    st.write(f'The result is {total} ')
+
